7571124c4afa69b64e081ec8a6fad5df7bdcf30f
Mihai Dzubaila <mihai.dzubaila@gmail.com>
2023-04-04 21:23:22 +0300

    BMS-1

---
+++ src/main/java/org/sda/bms/Main.java
@@ -0,0 +1,11 @@
+package org.sda.bms;
+
+import org.sda.bms.utils.SessionManager;
+
+public class Main {
+
+    public static void main(String[] args) {
+        SessionManager.getSessionFactory();
+    }
+
+}
