8ed4e3d8279d0b57635b7724a130eae33ac58836
Mihai Dzubaila <mihai.dzubaila@gmail.com>
2023-04-05 21:27:48 +0300

    BMS-5 Create abstract and generic repository

---
+++ src/main/java/org/sda/bms/repository/AuthorRepository.java
@@ -0,0 +1,6 @@
+package org.sda.bms.repository;
+
+import org.sda.bms.model.Author;
+
+public interface AuthorRepository<A> extends BaseRepository<Author>{
+}
---
+++ src/main/java/org/sda/bms/repository/BookRepository.java
@@ -0,0 +1,6 @@
+package org.sda.bms.repository;
+
+import org.sda.bms.model.Book;
+
+public interface BookRepository<B> extends BaseRepository<Book>{
+}
